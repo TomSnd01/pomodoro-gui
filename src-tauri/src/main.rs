b34ec488fa83d55a518ae628f4f74e3afdedd47e
TomSnd01 <tom.sander243@gmail.com>
2024-03-21 22:31:55 +0100

    set up rust backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,15 @@
 
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![command_test])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+mod home_page;
+mod settings_page;
+
+#[tauri::command]
+fn command_test() {
+  println!("Hello");
 }
--- src/components/home/controlButtons/buttonPlayPause.tsx
+++ src/components/home/controlButtons/buttonPlayPause.tsx
@@ -1,16 +1,23 @@
 import { PauseIcon } from "@/components/icons/home/PauseIcon";
 import { PlayIcon } from "@/components/icons/home/PlayIcon";
 import styles from "../page.module.css";
+import { invoke } from "@tauri-apps/api/tauri";
+import { useEffect } from "react";
 
 type CustomProps = {
   playing: boolean;
 };
+
+function handleClick() {
+  invoke("command_test", { name: "Next.js" }).catch(console.error);
+}
 
 export default function ButtonPlayPause({ playing }: CustomProps) {
   return (
     <button
       style={{ marginRight: 5, marginLeft: 5 }}
       className={styles.svgIcon}
+      onClick={handleClick}
     >
       {playing ? <PauseIcon size={34} /> : <PlayIcon size={34} />}
     </button>
